3cd754a68a714a66085e0310cf21c96514845ed3
Andrew Capp <acapp909@gmail.com>
2019-11-08 01:14:34 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -5,5 +5,9 @@ class Person
 :weight, :handed, :complexion, :t_shirt_size,
 :wrist_size, :glove_size, :pant_length, :pant_width
 
-
+  def initialize(arguments)
+    arguments.each do |key, value|
+      self.send("#{key}" = value)
+    end
+  end
 end
